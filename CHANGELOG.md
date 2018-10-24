05cb97cefe1fa69282dc3e4ffe372fb22fd6de82
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2018-10-24 19:50:00 -0300

    #36 push changelog changes on release preparation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -53,6 +53,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 [2.0.0]: https://github.com/pdacostaporto/representations/compare/1.2.0...2.0.0
 [1.2.0]: https://github.com/pdacostaporto/representations/compare/1.1.1...1.2.0
 [1.1.1]: https://github.com/pdacostaporto/representations/compare/1.0.0...1.1.1
-[1.0.0]: https://github.com/pdacostaporto/representations/compare/0.3...v1.0.0
+[1.0.0]: https://github.com/pdacostaporto/representations/compare/0.3...1.0.0
 [0.3]: https://github.com/pdacostaporto/representations/compare/0.2...0.3
-[0.2]: https://github.com/pdacostaporto/representations/compare/0.1...v0.2
+[0.2]: https://github.com/pdacostaporto/representations/compare/0.1...0.2
--- prepare.sh
+++ prepare.sh
@@ -8,5 +8,10 @@ chmod 600 deploy.pem
 ssh-add deploy.pem
 sed -i -e "s/\[Unreleased\]/\[$RELEASE_VERSION\]/" CHANGELOG.md
 sed -i -e "s/\.\.\.HEAD$/\.\.\.$RELEASE_VERSION/" CHANGELOG.md
-sed -n "/## \[$RELEASE_VERSION\]/,/## \[/{/## \[/b;p}" CHANGELOG.md
+git add CHANGELOG.md
+git commit -m "Changelog updated to version $RELEASE_VERSION"
 mvn --batch-mode -Dtag=$RELEASE_VERSION release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION-SNAPSHOT
+sed -i -e "/^\[$RELEASE_VERSION\]: https:.*/i \[Unreleased\]: https:\/\/github.com\/pdacostaporto\/representations\/compare\/$RELEASE_VERSION\.\.\.HEAD" CHANGELOG.md
+git add CHANGELOG.md
+git commit -m "Changelog updated for new development version"
+git push
