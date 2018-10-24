0a946b925f5ee0ae1c9464d1bf3c95d5e70cf3ae
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2018-10-24 20:11:44 -0300

    #36 import keyring

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+gpg --import keyring.gpg
 git config --global user.email "representations@kerri.uy"
 git config --global user.name "Travis CI"
 git checkout -B master
-git branch --set-upstream master origin/master
+git branch --set-upstream-to origin/master
 eval "$(ssh-agent -s)"
 chmod 600 deploy.pem
 ssh-add deploy.pem
@@ -11,7 +12,7 @@ sed -i -e "s/\[Unreleased\]/\[$RELEASE_VERSION\]/" CHANGELOG.md
 sed -i -e "s/\.\.\.HEAD$/\.\.\.$RELEASE_VERSION/" CHANGELOG.md
 git add CHANGELOG.md
 git commit -m "Changelog updated to version $RELEASE_VERSION"
-mvn --batch-mode -Dtag=$RELEASE_VERSION release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION-SNAPSHOT -Dgpg.skip
+mvn --batch-mode -Dtag=$RELEASE_VERSION release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION-SNAPSHOT --settings settings.xml
 sed -i -e "/^\[$RELEASE_VERSION\]: https:.*/i \[Unreleased\]: https:\/\/github.com\/pdacostaporto\/representations\/compare\/$RELEASE_VERSION\.\.\.HEAD" CHANGELOG.md
 git add CHANGELOG.md
 git commit -m "Changelog updated for new development version"
