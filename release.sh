2aa3f67f362bb95131a218fb2bc43344b43054b5
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2018-10-25 01:33:28 -0300

    #36 added scm url and tag to release config

--- release.sh
+++ release.sh
@@ -8,6 +8,8 @@ ssh-add deploy.pem
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote add github git@github.com:pdacostaporto/representations.git
+echo "scm.url=scm:git:ssh://git@github.com/$TRAVIS_REPO_SLUG.git
+scm.tag=$TRAVIS_TAG" > release.properties
 mvn --batch-mode release:perform --settings settings.xml
 jq -n \
   --arg body "`sed -n "/## \[$TRAVIS_TAG\]/,/## \[/{/## \[/b;p}" CHANGELOG.md`" \
