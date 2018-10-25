efafbb3e252376efaafd526c1d7568980be63a50
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2018-10-25 00:04:50 -0300

    #36 push updated changelog and delete release branch

--- prepare.sh
+++ prepare.sh
@@ -5,12 +5,10 @@ chmod 600 secret.gpg
 chmod 600 public.gpg
 eval "$(ssh-agent -s)"
 ssh-add deploy.pem
-git config --global user.email "representations@kerri.uy"
+git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote set-url origin git@github.com:pdacostaporto/representations.git
-git fetch
+git remote add github git@github.com:pdacostaporto/representations.git
 git checkout -B master
-git branch --set-upstream-to origin/master
 sed -i -e "s/\[Unreleased\]/\[$RELEASE_VERSION\]/" CHANGELOG.md
 sed -i -e "s/\.\.\.HEAD$/\.\.\.$RELEASE_VERSION/" CHANGELOG.md
 git add CHANGELOG.md
@@ -20,4 +18,5 @@ mvn --batch-mode -Dtag=$RELEASE_VERSION release:prepare -DreleaseVersion=$RELEAS
 sed -i -e "/^\[$RELEASE_VERSION\]: https:.*/i \[Unreleased\]: https:\/\/github.com\/pdacostaporto\/representations\/compare\/$RELEASE_VERSION\.\.\.HEAD" CHANGELOG.md
 git add CHANGELOG.md
 git commit -m "Changelog updated for new development version"
-git push
+git push github master
+git push --delete github release
